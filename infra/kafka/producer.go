5a63864605d76ed78eb4d25dd4978d50fc003fe7
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-04-05 22:18:34 -0300

    refactor: fixing gramatic

--- infra/kafka/producer.go
+++ infra/kafka/producer.go
@@ -18,7 +18,7 @@ func NewKafkaProducer() *ckafka.Producer {
 	return p
 }
 
-// Publish is simple function created to publish new message to kafka
+// Publish is a simple function created to publish new message to kafka
 func Publish(msg string, topic string, producer *ckafka.Producer) error {
 	message := &ckafka.Message{
 		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
