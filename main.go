19e488a68c133040459d623f14d84e60366b5038
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-04-05 22:26:43 -0300

    feat: add environment variables

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt"
 	route2 "github.com/xumes/fullcycle-emulador/application/route"
+	"github.com/joho/godotenv"
+	"log"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	route := route2.Route{
