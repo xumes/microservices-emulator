7a2f206f0c96b0fbbae06d08cf0d16051cf8cd4c
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-04-05 22:15:24 -0300

    refactor: improving kafka consumer

--- infra/kafka/consumer.go
+++ infra/kafka/consumer.go
@@ -1,35 +1,40 @@
 package kafka
 
 import (
-	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
-	"os"
-	"log"
 	"fmt"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"os"
 )
 
-type KafkaConsomer struct {
-	MsgChan chan *ckafka.Messsage
+// KafkaConsumer holds all consumer logic and settings of Apache Kafka connections/
+// Also has a Message channel which is a channel where the messages are going to be pushed
+type KafkaConsumer struct {
+	MsgChan chan *ckafka.Message
 }
 
-
-func(k *KafkaConsomer) Consume() {
-	configMap := &ckafka.configMap{
-		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
-		"group.id": os.Getenv("KafkaConsumerGroupdId"),
+// NewKafkaConsumer creates a new KafkaConsumer struct with its message channel as dependency
+func NewKafkaConsumer(msgChan chan *ckafka.Message) *KafkaConsumer {
+	return &KafkaConsumer{
+		MsgChan: msgChan,
 	}
+}
 
+// Consume consumes all message pulled from apache kafka and sent it to message channel
+func (k *KafkaConsumer) Consume() {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":          os.Getenv("KafkaConsumerGroupId"),
+	}
 	c, err := ckafka.NewConsumer(configMap)
 	if err != nil {
 		log.Fatalf("error consuming kafka message:" + err.Error())
 	}
-
-	topics := []string{os.Getenv("kafkaReadTopic")}
+	topics := []string{os.Getenv("KafkaReadTopic")}
 	c.SubscribeTopics(topics, nil)
-
 	fmt.Println("Kafka consumer has been started")
-
 	for {
-		msg, err := c.ReadMessage( -1)
+		msg, err := c.ReadMessage(-1)
 		if err == nil {
 			k.MsgChan <- msg
 		}
